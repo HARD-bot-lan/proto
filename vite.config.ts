62248d688b25da65dcb9872782376b882e67f797
HARD-bot-lan <chillman705@gmail.com>
2025-02-24 01:00:59 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/proto/", // Move this to the root level
+  
   server: {
     host: "::",
     port: 8080,
