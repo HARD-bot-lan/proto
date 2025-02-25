b1adcb7098089e57218a4263380af595277e717a
HARD-bot-lan <chillman705@gmail.com>
2025-02-25 21:40:36 +0000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -286,7 +286,7 @@ const Index = () => {
         <div className="inline-flex items-center gap-2 text-white hover:text-primary-200 transition-colors">
           <Mail className="w-5 h-5" />
           <a href="mailto:emminovtech@emi.ac.ma" className="hover:underline">
-            cit-eminovtech@emi.ac.ma
+            ci-eminovtech@emi.ac.ma
           </a>
         </div>
       </div>
